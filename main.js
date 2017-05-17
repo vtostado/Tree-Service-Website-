218b250144444aa7957483f1de98351893cd2db1
vtostado <vanessatostado@Vanessas-MBP.stu-wireless.wesleyan.edu>
2017-05-16 23:16:13 -0400

    draft 2 of website, some components still unimplemented

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-var tabcontent = document.getElementsByClassName('tabcontent');
-var tablinks = document.getElementsByClassName("tablinks");
-console.log(tablinks)
-
-
-function showContent (e,page) {
-	
-    
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(page).style.display = "block";
-    e.currentTarget.className += " active";
-	
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('#dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
